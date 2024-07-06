dc3059c92ccc5db6b50760b25071cf99c737f5e0
zahra <zahra.annisa.fakhira.tik22@gmail.com>
2024-07-06 17:36:11 +0700

    Bottom Navigation

--- app/src/main/java/com/example/wilsonpreschool/MainActivity.java
+++ app/src/main/java/com/example/wilsonpreschool/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.wilsonpreschool;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.MenuItem;
+import com.example.wilsonpreschool.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.home) {
+                    replaceFragment(new HomeFragment());
+                } else if (item.getItemId() == R.id.programs) {
+                    replaceFragment(new ProgramsFragment());
+                } else if (item.getItemId() == R.id.registration) {
+                    replaceFragment(new RegistrationFragment());
+                } else if (item.getItemId() == R.id.blog) {
+                    replaceFragment(new BlogFragment());
+                } else if (item.getItemId() == R.id.contact_us) {
+                    replaceFragment(new ContactFragment());
+                } else {
+                    return false;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
