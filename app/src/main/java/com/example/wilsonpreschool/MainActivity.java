16d751d92849efa07efc92d95e391bc4d6fdeebd
zahra <zahra.annisa.fakhira.tik22@gmail.com>
2024-07-06 19:15:51 +0700

    Menu Item

--- app/src/main/java/com/example/wilsonpreschool/MainActivity.java
+++ app/src/main/java/com/example/wilsonpreschool/MainActivity.java
@@ -6,13 +6,34 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
 import com.example.wilsonpreschool.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
     ActivityMainBinding binding;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.account) {
+            Toast.makeText(this, "You have clicked on account", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (item.getItemId() == R.id.logout) {
+            Toast.makeText(this, "You have clicked on logout", Toast.LENGTH_SHORT).show();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
